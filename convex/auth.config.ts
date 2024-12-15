ad6964d2a579d011355d24a59fcfe74b4f538139
Jackslu1050 <jackslumbu@hotmail.be>
2024-12-15 13:10:50 +0100

    setting up clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+    providers: [
+      {
+        domain: "https://nice-gannet-47.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
