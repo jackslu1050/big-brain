66a17a915feff45adfe2b833ceeef032162b611b
Jackslu1050 <jackslumbu@hotmail.be>
2024-12-15 16:30:08 +0100

    Scoping data to user identifier

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,50 +1,58 @@
 import {
-    MutationCtx,
-    QueryCtx,
-    action,
-    internalAction,
-    internalMutation,
-    internalQuery,
-    mutation,
-    query,
-  } from "./_generated/server";
-  import { ConvexError, v } from "convex/values";
-  import { internal } from "./_generated/api";
+  MutationCtx,
+  QueryCtx,
+  action,
+  internalAction,
+  internalMutation,
+  internalQuery,
+  mutation,
+  query,
+} from "./_generated/server";
+import { ConvexError, v } from "convex/values";
+import { internal } from "./_generated/api";
 
-  export const getDocuments=query({
-    async handler(ctx){
-      return await ctx.db.query("documents").collect()
+export const getDocuments = query({
+  async handler(ctx) {
+    // return await ctx.db.query("documents").collect();
+    const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
+    if (!userId) {
+      return [];
     }
-  })
+    return await ctx.db
+      .query("documents")
+      .withIndex("by_tokenIdentifier", (q) => q.eq("tokenIdentifier", userId)).collect();
+  },
+});
 
 export const createDocument = mutation({
-    args: {
-      title: v.string(),
+  args: {
+    title: v.string(),
     //   fileId: v.id("_storage"),
-      orgId: v.optional(v.string()),
-    },
-    async handler(ctx, args) {
-    //   const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
-  
-    //   if (!userId) {
-    //     throw new ConvexError("Not authenticated");
-    //   }
-  
+    orgId: v.optional(v.string()),
+  },
+  async handler(ctx, args) {
+    const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
+    console.log(userId);
+    if (!userId) {
+      throw new ConvexError("Not authenticated");
+    }
+
     //   let documentId: Id<"documents">;
-  
+
     //   if (args.orgId) {
     //     const isMember = await hasOrgAccess(ctx, args.orgId);
     //     if (!isMember) {
     //       throw new ConvexError("You do not have access to this organization");
     //     }
-  
-        // documentId = 
-        await ctx.db.insert("documents", {
-          title: args.title,
-        //   fileId: args.fileId,
-        //   description: "",
-        //   orgId: args.orgId,
-        });
+
+    // documentId =
+    await ctx.db.insert("documents", {
+      title: args.title,
+      tokenIdentifier: userId,
+      //   fileId: args.fileId,
+      //   description: "",
+      //   orgId: args.orgId,
+    });
     //   } else {
     //     documentId = await ctx.db.insert("documents", {
     //       title: args.title,
@@ -53,7 +61,7 @@ export const createDocument = mutation({
     //       description: "",
     //     });
     //   }
-  
+
     //   await ctx.scheduler.runAfter(
     //     0,
     //     internal.documents.generateDocumentDescription,
@@ -62,6 +70,5 @@ export const createDocument = mutation({
     //       documentId,
     //     }
     //   );
-    },
-  });
-  
+  },
+});
